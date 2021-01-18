d69f68000ae3264c346177776c7e7b3ab2a5b16b
brendonstahl97 <brendonstahl97@gmail.com>
2021-01-18 16:23:52 -0500

    html routes are working

---
+++ server.js
@@ -0,0 +1,36 @@
+// requiring depencies
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+
+// PORT
+
+const PORT = process.env.PORT || 3000 ;
+
+
+// defining app as express
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+// connecting with MongoDB via mongoose at our myapp database running locally on default port 27017
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+
+require("./routes/htmlRoutes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+
+app.listen(PORT, () => {
+
+    console.log(`Application is running on http://localhost:${PORT}`);
+
+});
